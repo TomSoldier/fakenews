13a4718bb3bc754ec5a63329475b45d95cf8cb15
Nandor Drexler <wawe.freestyle7@gmail.com>
2021-11-21 16:36:53 +0100

    Token refactor

---
+++ src/Backend/FakeNews/FakeNews.Common/Roles/ClaimTypes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeNews.Common.Roles
+{
+    public static class ClaimTypes
+    {
+        public const string Name = "name";
+        public const string Role = "role";
+        public const string Email = "email";
+    }
+}

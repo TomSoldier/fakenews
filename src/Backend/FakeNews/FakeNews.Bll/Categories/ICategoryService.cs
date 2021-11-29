26976fa26186bd2e9d7db2166bcc1ae2cfa0ef47
Tamás Katona <katona.tamas.97@gmail.com>
2021-11-29 04:08:08 +0100

    Removed unnecessary things.

--- src/Backend/FakeNews/FakeNews.Bll/Categories/ICategoryService.cs
+++ src/Backend/FakeNews/FakeNews.Bll/Categories/ICategoryService.cs
@@ -1,8 +1,5 @@
 ﻿using FakeNews.Transfer.Categories;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace FakeNews.Bll.Categories
--- src/Backend/FakeNews/FakeNews.Bll/Users/IUserService.cs
+++ src/Backend/FakeNews/FakeNews.Bll/Users/IUserService.cs
@@ -1,9 +1,5 @@
 ﻿using FakeNews.Transfer.Jwt;
 using FakeNews.Transfer.Users;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace FakeNews.Bll.Users
